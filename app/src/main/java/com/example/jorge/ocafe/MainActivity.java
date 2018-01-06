68ce92b7cbe45688db0b493e2a0c9c56b243fec2
jorge <mistahjorge@gmail.com>
2018-01-06 15:53:11 +0000

    Realização do projeto através do Guia Prático "Aplicação News" - Passo 4

--- app/src/main/java/com/example/jorge/ocafe/MainActivity.java
+++ app/src/main/java/com/example/jorge/ocafe/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jorge.ocafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate called.", Toast.LENGTH_SHORT).show();
+
+        if (findViewById(R.id.fragment_container_portrait) != null) {
+            if (productDescriptionFragment != null) {
+                return;
+            }
+            productDescriptionFragment = new ProductDescriptionFragment();
+            productDescriptionFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_portrait, productDescriptionFragment).commit();
+            Toast.makeText(this, "Fragment Created.", Toast.LENGTH_SHORT).show();
+        }
 
         if (findViewById(R.id.fragment_container_land1) != null) {
             if (productsListFragment != null) {
