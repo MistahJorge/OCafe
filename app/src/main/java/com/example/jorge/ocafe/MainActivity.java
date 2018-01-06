8ee7a04f6499a560552df0b99494a163f0b878b4
jorge <mistahjorge@gmail.com>
2018-01-06 15:34:21 +0000

    Realização do projeto através do Guia Prático "Aplicação News" - Passos 3 É importante notar que estou utilizando fragmentos dinâmicos em vez de estáticos, e que criei um fragmento a mais, porque será util mais tarde

--- app/src/main/java/com/example/jorge/ocafe/MainActivity.java
+++ app/src/main/java/com/example/jorge/ocafe/MainActivity.java
@@ -5,9 +5,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProductListFragment productsListFragment;
+    ProductDescriptionFragment productDescriptionFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container_land1) != null) {
+            if (productsListFragment != null) {
+                return;
+            }
+            productsListFragment = new ProductListFragment();
+            productsListFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_land1, productsListFragment).commit();
+        }
+
+        if (findViewById(R.id.fragment_container_land2) != null) {
+            if (productDescriptionFragment != null) {
+                return;
+            }
+            productDescriptionFragment = new ProductDescriptionFragment();
+            productDescriptionFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_land2, productDescriptionFragment).commit();
+        }
     }
 }
