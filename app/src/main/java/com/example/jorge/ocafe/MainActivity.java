e37a5304358fee02763c23a3a391b7c78947d4e7
jorge <mistahjorge@gmail.com>
2018-01-13 18:05:47 +0000

    Realização do projeto através do Guia Prático "Aplicação News" - Passo 5

--- app/src/main/java/com/example/jorge/ocafe/MainActivity.java
+++ app/src/main/java/com/example/jorge/ocafe/MainActivity.java
@@ -6,6 +6,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String[] names = {
+            "Coffe",
+            "Cookie"
+    };
+    public static String[] descriptions = {
+            "Article One\n\nExcepteur pour-over occaecat squid biodiesel umami gastropub, nulla " +
+                    "laborum salvia dreamcatcher fanny pack. Ullamco culpa retro ea, trust fund " +
+                    "excepteur eiusmod direct trade banksy nisi lo-fi cray messenger bag. " +
+                    "Nesciunt esse carles selvage put a bird on it gluten-free, wes anderson ut " +
+                    "trust fund twee occupy viral. Laboris small batch scenester pork belly, " +
+                    "leggings ut farm-to-table aliquip yr nostrud iphone viral next level. Craft " +
+                    "beer dreamcatcher pinterest truffaut ethnic, authentic brunch. Esse " +
+                    "single-origin coffee banksy do next level tempor. Velit synth dreamcatcher, " +
+                    "magna shoreditch in american apparel messenger bag narwhal PBR ennui " +
+                    "farm-to-table.",
+            "Article Two\n\nVinyl williamsburg non velit, master cleanse four loko banh mi. Enim " +
+                    "kogi keytar trust fund pop-up portland gentrify. Non ea typewriter dolore " +
+                    "deserunt Austin. Ad magna ethical kogi mixtape next level. Aliqua pork " +
+                    "belly thundercats, ut pop-up tattooed dreamcatcher kogi accusamus photo " +
+                    "booth irony portland. Semiotics brunch ut locavore irure, enim etsy laborum " +
+                    "stumptown carles gentrify post-ironic cray. Butcher 3 wolf moon blog synth, " +
+                    "vegan carles odd future."
+    };
+
     ProductListFragment productsListFragment;
     ProductDescriptionFragment productDescriptionFragment;
 
@@ -14,7 +38,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate called.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, names[0], Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, descriptions[0], Toast.LENGTH_LONG).show();
+
+        Toast.makeText(this, names[1], Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, descriptions[1], Toast.LENGTH_LONG).show();
 
         if (findViewById(R.id.fragment_container_portrait) != null) {
             if (productDescriptionFragment != null) {
